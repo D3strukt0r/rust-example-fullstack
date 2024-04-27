659127b47f8a14db3341f5c8de10b3753518ea73
D3strukt0r <dev@d3strukt0r.me>
2024-04-27 10:38:43 +0200

    Add json endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,17 @@
-use axum::{extract::Path, response::IntoResponse, routing::get, Router};
+use axum::{extract::Path, response::IntoResponse, routing::get, Router, Json};
+use serde::{Deserialize, Serialize};
+use axum::routing::post;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/:name", get(hello_name))
+        .route("/your-route", post(workshop_echo));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
 
 async fn hello_world() -> impl IntoResponse {
     "Hello World"
@@ -8,12 +21,12 @@ async fn hello_name(Path(name): Path<String>) -> impl IntoResponse {
     format!("Hello {name}")
 }
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/", get(hello_world))
-        .route("/:name", get(hello_name));
+#[derive(Serialize, Deserialize)]
+struct Workshop {
+    attendees_count: i32,
+    people_like_it: bool,
+}
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+async fn workshop_echo(Json(workshop): Json<Workshop>) -> impl IntoResponse {
+    Json(workshop)
 }
