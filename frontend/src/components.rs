7848b29128fc4f3cd20fa1d25fb4f2eb8ca9d792
D3strukt0r <dev@d3strukt0r.me>
2024-05-03 20:03:43 +0200

    14. Frontend: Load or Create - that is the Question

--- frontend/src/components.rs
+++ frontend/src/components.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
-use model::{PostShopItem};
-use crate::controllers::{delete_item, get_items, post_item};
+use model::PostShopItem;
+use crate::controllers::{delete_item, get_items, post_item, create_list};
 
 #[allow(non_snake_case)]
 pub fn App() -> Element {
@@ -13,7 +13,9 @@ pub fn App() -> Element {
 enum Route {
     #[layout(Layout)]
         #[route("/")]
-        Home {},
+        LoadOrCreateList {},
+        #[route("/list/:list_uuid")]
+        Home { list_uuid: String },
         #[route("/profile")]
         Profile {}
 }
@@ -26,7 +28,7 @@ pub fn Layout() -> Element {
             div {
                 class: "navbar flex",
                 div {
-                    Link { class: "p-4", to: Route::Home{}, "Home" }
+                    Link { class: "p-4", to: Route::LoadOrCreateList{}, "Home" }
                     Link { class: "p-4", to: Route::Profile{}, "Profile" }
                 }
             }
@@ -38,18 +40,78 @@ pub fn Layout() -> Element {
 }
 
 #[component]
-pub fn Home() -> Element {
-    let list_uuid = use_signal(|| "9e137e61-08ac-469d-be9d-6b3324dd20ad".to_string());
-    let change_signal = use_signal(|| ListChanged);
-    let rust_basel = "Rust Basel";
+pub fn LoadOrCreateList() -> Element {
+    let nav = use_navigator();
+    let mut list_uuid = use_signal(|| "".to_string());
+
+    let onloadsubmit = move |_| {
+        spawn({
+            async move {
+                let uuid_value = list_uuid.read().clone();
+                if !uuid_value.is_empty() {
+                    nav.push(Route::Home {
+                        list_uuid: uuid_value,
+                    });
+                }
+            }
+        });
+    };
+
+    let on_create_list_click = move |_| {
+        let nav = nav.clone();
+        spawn({
+            async move {
+                let response = create_list().await;
+                if let Ok(created_list) = response {
+                    nav.push(Route::Home {
+                        list_uuid: created_list.uuid,
+                    });
+                }
+            }
+        });
+    };
+
     rsx! {
-        h1{
-            "Welcome to {rust_basel}"
+        div{
+            class: "grid place-content-evently grid-cols-1 md:grid-cols-2 w-full gap-4",
+            div {
+                class: "card glass min-h-500 flex flex-col content-end gap-4 p-4",
+                button{
+                    class: "btn btn-primary",
+                    onclick: on_create_list_click,
+                    "Create new List"
+                }
+            }
+            div { class: "card glass min-h-500",
+                form {
+                    onsubmit: onloadsubmit,
+                    div {
+                        class: "flex flex-col gap-4 p-4",
+                        input{
+                            class:"input input-bordered",
+                            r#type:"text",
+                            placeholder:"Enter UUID here...",
+                            id: "uuid",
+                            name: "uuid",
+                            oninput: move |e| list_uuid.set(e.data.value())
+                        }
+                        button{
+                            class: "btn btn-primary",
+                            r#type: "submit",
+                            "Load existing List"
+                        }
+                    }
+                }
+            }
         }
-        button{
-            class: "btn",
-            "My stylish button"
-        }
+    }
+}
+
+#[component]
+pub fn Home(list_uuid: String) -> Element {
+    let list_uuid = use_signal(|| list_uuid);
+    let change_signal = use_signal(|| ListChanged);
+    rsx! {
         ShoppingList{list_uuid, change_signal}
         ItemInput{list_uuid, change_signal}
     }
